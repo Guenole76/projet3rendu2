0df913836a720d17c30ccdd1b770bf9a144ac727
Guenole76 <Gunol>
2020-04-17 23:32:19 +0200

    cedric

---
+++ controller/controller.php
@@ -0,0 +1,11 @@
+<?php
+require('model/model.php');
+
+function quiz()
+{
+	//On appelle la fonction de récupération qui est dans le model et on met le résultat renvoyé dans la variable quizData
+	
+	//On appelle la vue
+    require('view/quizView.php');
+   
+}
